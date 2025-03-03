5c86272cbbe3aea9af7e540a6fa885c01e35e251
GamingRoos <landlcopley@gmail.com>
2025-03-03 23:27:06 +1100

    Actually built firmware

--- Firmware/Vial/keymaps/vial/config.h
+++ Firmware/Vial/keymaps/vial/config.h
@@ -3,3 +3,7 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0x71, 0xB3, 0x61, 0x49, 0xBE, 0xA4, 0x49, 0xBD}
+
+#define VIAL_TAP_DANCE_ENTRIES 2
+#define VIAL_COMBO_ENTRIES 3
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
